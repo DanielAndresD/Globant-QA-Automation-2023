21d5f73bd79a75f9d3ad43849afca8cab1d93c49
DanielDavila <dadavilal@correo.udistrital.edu.co>
2023-05-24 09:41:30 -0500

    holamundo con java

---
+++ JAVA/clase 27/mi_primer_proyecto/src/holamundo.java
@@ -0,0 +1,16 @@
+/**
+ * Este programa escribe Hola Mundo en consola
+ * utilizando el método System.out.println
+ * @author  Daniel Andres Davila Lesmes
+ * {@link} https://www.linkedin.com/in/danielandresd/
+ * @version 1.0
+ */
+public class holamundo {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
